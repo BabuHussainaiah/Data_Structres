517a48637d6f17ab3c703bee2a4a111696ec5900
BabuHussainaiah <60291362+BabuHussainaiah@users.noreply.github.com>
2020-02-02 21:13:31 +0530

    Del_at_end of the list
    write a program to delete at of the list  by single linked list

---
+++ sin_del_end.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node *link;
+};
+struct node *start=NULL;
+void delete_at_end();
+void create_list_at_end(int *,int);
+void display();
+#define A_ELE 4
+int count=0;
+void main()
+{
+	int num;
+	int lal=10;
+	int arr[A_ELE],i;
+	while(1)
+	{
+		printf("enter array element  (0 FOR EXIT)\n");
+		scanf("%d",&num);
+		if(num==0)
+			exit(0);
+		printf("enter array elements\n");
+			for(i=0;i<A_ELE;i++)
+				scanf("%d",&arr[i]);
+		create_list_at_end(arr,A_ELE);
+		delete_at_end();
+		display();
+	}
+}
+
+void delete_at_end()
+{
+	struct node *ptr,*temp;
+	//ptr=start;
+	if(start==NULL)
+	{
+		printf("list is empty\n");
+		return;
+	}
+	ptr=start;
+	if(start->link==NULL)
+	{
+		free(ptr);
+		start=NULL;
+		return;
+	}
+	//ptr=start;
+	while(ptr->link!=NULL)
+	{
+		temp=ptr;
+		ptr=ptr->link;
+		//free(ptr);
+		//temp->link=NULL;
+	}
+	free(ptr);
+	temp->link=NULL;
+}
+
+void display()
+{
+	struct node *temp;
+	if(start==NULL)
+	{
+		printf("list is empty\n");
+		return;
+	}
+	printf("Array Elements Are : \n");
+	temp=start;
+	while(temp!=NULL)
+	{
+		printf("%d \n",temp->data);
+		temp=temp->link;
+	}
+}
+void create_list_at_end(int *iptr,int n)
+{
+	int i;
+	struct node *l_start,*temp;
+	struct node *ptr=(struct node *)malloc(1*sizeof(struct node));
+	ptr->data=iptr[0];
+	ptr->link=NULL;
+	l_start=temp=ptr;
+	for(i=1;i<n;i++)
+	{
+		ptr=(struct node *)malloc(1*sizeof(struct node));
+		ptr->data=iptr[i];
+		ptr->link=temp->link;
+		temp->link=ptr;
+		temp=ptr;
+	}
+	if(start==NULL)
+	{
+		start=l_start;
+		return;
+	}
+	temp=start;
+	while(temp->link!=NULL)
+	{
+		temp=temp->link;
+	}
+	temp->link=l_start;
+	
+}
+
